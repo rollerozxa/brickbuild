ae833204ea1968bd2a077f06773d2b388466219b
ROllerozxa <rollerozxa@voxelmanip.se>
2023-11-09 21:48:27 +0100

    Add pickblock and don't give node drops

--- mods/brickbuild/init.lua
+++ mods/brickbuild/init.lua
@@ -17,6 +17,12 @@ minetest.register_item(':', {
 			},
 		}
 	},
+	on_place = function(itemstack, placer, pointed_thing)
+		if minetest.is_creative_enabled(placer:get_player_name()) then
+			local pointed_node = minetest.get_node(pointed_thing.under)
+			return pointed_node
+		end
+	end
 })
 
 minetest.register_on_placenode(function(pos, newnode, placer, oldnode, itemstack, pointed_thing)
@@ -26,10 +32,27 @@ minetest.register_on_placenode(function(pos, newnode, placer, oldnode, itemstack
 	end
 end)
 
+local old_handle_node_drops = minetest.handle_node_drops
+function minetest.handle_node_drops(pos, drops, digger)
+	if not digger or not digger:is_player() or
+		not minetest.is_creative_enabled(digger:get_player_name()) then
+		return old_handle_node_drops(pos, drops, digger)
+	end
+	local inv = digger:get_inventory()
+	if inv then
+		for _, item in ipairs(drops) do
+			if not inv:contains_item("main", item, true) then
+				inv:add_item("main", item)
+			end
+		end
+	end
+end
+
 local function include(file)
 	dofile(minetest.get_modpath(minetest.get_current_modname()) .. "/"..file..".lua")
 end
-include('baseplate')
 include('parts')
 include('player')
 include('player_api')
+
+include('baseplate')
